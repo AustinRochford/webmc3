49a12b2b4a7f6c1c5e832b82fee2e1322d6a56f3
AustinRochford <arochford@monetate.com>
2018-01-02 09:04:42 -0500

    Add url-based router

--- webmc3/app.py
+++ webmc3/app.py
@@ -1,12 +1,14 @@
 import dash
+import dash_core_components as dcc
 import dash_html_components as html
 
-from .univariate import layout as univariate_layout
+from . import univariate
 
 LOGO = html.Img(
     src="https://camo.githubusercontent.com/2af4bb9d3ff6744a6ad1aab0b2b916b5efee8b49/68747470733a2f2f63646e2e7261776769742e636f6d2f70796d632d646576732f70796d63332f6d61737465722f646f63732f6c6f676f732f7376672f50794d43335f62616e6e65722e737667",
     width=300
 )
+
 
 
 def webmc3_app(trace):
@@ -19,8 +21,20 @@ def webmc3_app(trace):
     app.config.suppress_callback_exceptions = True
 
     app.layout = html.Div([
+        dcc.Location(id='url', refresh=False),
         html.Div([LOGO, html.Hr()]),
-        univariate_layout(app, trace)
+        html.Div(id='page-content')
     ])
+
+    univariate_layout = univariate.layout(app, trace)
+
+    @app.callback(
+        dash.dependencies.Output('page-content', 'children'),
+        [dash.dependencies.Input('url', 'pathname')]
+    )
+    def update_page_content(pathname):
+        if pathname in ['/', '/univariate']:
+            return univariate_layout
+    
     
     return app
